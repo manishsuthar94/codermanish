2599a284bc3e944516c4a6b385030f38113eac8b
manishsuthar94 <manishsutharr94@gmail.com>
2024-08-13 17:19:50 +0530

    contactpage

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -18,65 +18,65 @@ export default function Page() {
   return (
     <div className="py-20">
       <div className="text-center mb-8">
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-4xl font-bold">
           Contact Me<span className="text-primary">.</span>
         </h1>
-        <p className="text-xs">
-          Feel free to contact me if you have any <br /> questions or works
+        <p className="text-xs mt-3">
+          Feel free to reach out if you have any <br /> questions or potential
+          projects.
           <span className="text-primary">.</span>
         </p>
-        <section className="relative grid place-content-center py-8 px-6">
-          <div className="md:max-w-2xl max-w-md text-center grid gap-1">
-            <h1 className="md:text-4xl lg:text-3xl text-3xl font-bold">
-              Email:{" "}
-              <span className="underline underline-offset-4 text-primary">
-                manishsutharr94@gmail.com
-              </span>
-            </h1>
-            <h1 className="md:text-4xl lg:text-3xl text-3xl font-bold py-4">
-              Phone:{" "}
-              <span className="underline underline-offset-4 text-primary">
-                8866281718
-              </span>
-            </h1>
 
-            <div className="flex gap-2 items-center justify-center bg-secondary rounded-full p-4 w-fit mx-auto">
-              <Link
-                href="https://github.com/manishsuthar94/"
-                className="hover:text-primary"
-                target="_blank"
-              >
-                <GitHubLogoIcon className="h-7 w-7 m-1" />
-              </Link>
-              <Link
-                href="https://www.instagram.com/_mnis_94/"
-                className="hover:text-primary"
-                target="_blank"
-              >
-                <InstagramLogoIcon className="h-7 w-7 m-1" />
-              </Link>
-              <Link
-                href="https://www.linkedin.com/in/manish-suthar-8395b4311/"
-                className="hover:text-primary"
-                target="_blank"
-              >
-                <LinkedInLogoIcon className="h-7 w-7 m-1" />
-              </Link>
-              <Link
-                href="https://x.com/ManishSuthar_94"
-                className="hover:text-primary"
-                target="_blank"
-              >
-                <TwitterLogoIcon className="h-7 w-7 m-1" />
-              </Link>
-            </div>
-
-            <p className="text-[0.8rem] max-w-md mx-auto py-5">
-              An extensive display of my full-stack development skills,
-              experiences, and projects, demonstrating my proficiency and
-              commitment to coding
+        <section className="relative grid place-content-center py-11 px-6">
+          <div className="md:max-w-2xl max-w-md text-center grid gap-4">
+            <h1 className="md:text-3xl lg:text-2xl text-lg font-bold">
+              If you'd like to get in touch, you can email me at{" "}
+              <span className="underline underline-offset-4 text-primary">
+                manishsutharr94@gmail.com{" "}
+              </span>{" "}
+              or call me at{" "}
+              <span className="underline underline-offset-4 text-primary">
+                8866281718{" "}
+              </span>
               <span className="text-primary">.</span>
+            </h1>
+
+            <p className="text-[1.1rem] mx-auto leading-relaxed">
+              I'm always open to discussing new projects, creative ideas, or
+              opportunities to be part of your vision. Whether you have a
+              question, want to collaborate, or simply want to connect, I'd love
+              to hear from you!
             </p>
+          </div>
+          <div className="flex items-center justify-center gap-3 mt-5">
+            <Link
+              href="https://github.com/manishsuthar94/"
+              className="hover:text-primary"
+              target="_blank"
+            >
+              <GitHubLogoIcon className="h-7 w-7 m-1" />
+            </Link>
+            <Link
+              href="https://www.instagram.com/_mnis_94/"
+              className="hover:text-primary"
+              target="_blank"
+            >
+              <InstagramLogoIcon className="h-7 w-7 m-1" />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/manish-suthar-8395b4311/"
+              className="hover:text-primary"
+              target="_blank"
+            >
+              <LinkedInLogoIcon className="h-7 w-7 m-1" />
+            </Link>
+            <Link
+              href="https://x.com/ManishSuthar_94"
+              className="hover:text-primary"
+              target="_blank"
+            >
+              <TwitterLogoIcon className="h-7 w-7 m-1" />
+            </Link>
           </div>
         </section>
       </div>
