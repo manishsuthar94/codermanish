adf3de58162fb7604a03ad782608d5740054c3b7
manishsuthar94 <manishsutharr94@gmail.com>
2024-08-14 14:54:05 +0530

    logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 
 export default function Logo() {
-    return(
-        <Link href="/">
-            <h1 className="text-4xl font-bold select-none">Ms<span className="text-primary">.</span></h1>
-        </Link>
-    )
+  return (
+    <Link href="/">
+      <h1 className="text-2xl font-bold select-none">
+        Coder<span className="text-primary">.</span>MS
+        <span className="text-primary">.</span>
+      </h1>
+    </Link>
+  );
 }
