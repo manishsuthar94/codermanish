96020c4a21540669be2ddff5752c91d496617719
Manish Suthar <manishsutharr94@gmail.com>
2024-12-19 11:07:01 +0530

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,8 +4,7 @@ export default function Logo() {
   return (
     <Link href="/">
       <h1 className="text-2xl font-bold select-none">
-        Coder<span className="text-primary">.</span>MS
-        <span className="text-primary">.</span>
+        MS<span className="text-primary">.</span>
       </h1>
     </Link>
   );
