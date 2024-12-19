06aaf6cb00e6081cb02e170c50d3a218c72242cb
Manish Suthar <manishsutharr94@gmail.com>
2024-12-19 11:08:27 +0530

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/">
-      <h1 className="text-2xl font-bold select-none">
+      <h1 className="text-3xl font-bold select-none">
         MS<span className="text-primary">.</span>
       </h1>
     </Link>
