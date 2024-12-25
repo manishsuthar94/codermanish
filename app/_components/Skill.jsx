7bd8ef401ff3e66e56d9be697c54263581594917
Manish Suthar <manishsutharr94@gmail.com>
2024-12-25 11:02:51 +0530

    Update Skill.jsx

--- app/_components/Skill.jsx
+++ app/_components/Skill.jsx
@@ -8,11 +8,14 @@ import { Tooltip } from "@nextui-org/tooltip";
 
 export default function Skills() {
   const skills = [
-    { Icon: FaHtml5, color: "#dd4b24", name: "HTML" },
-    { Icon: IoLogoCss3, color: "#2862e9", name: "CSS" },
-    { Icon: SiBootstrap, color: "#7952B3", name: "Bootstrap" },
     { Icon: SiJavascript, color: "#ecda1d", name: "JavaScript" },
     { Icon: FaReact, color: "#5ed3f3", name: "React" },
+    { Icon: SiBootstrap, color: "#7952B3", name: "Bootstrap" },
+    { Icon: FaHtml5, color: "#dd4b24", name: "HTML" },
+    { Icon: IoLogoCss3, color: "#2862e9", name: "CSS" },
+    
+   
+   
   ];
 
   return (
